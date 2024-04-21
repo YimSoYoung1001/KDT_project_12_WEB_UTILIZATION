de7306767f711d9dc6e9ad729b21e93ca6f6f733
YeongshinOk <155431498+YeongshinOk@users.noreply.github.com>
2024-04-21 20:33:19 +0900

    work

--- Yeongshin/Team5_web/model.py
+++ Yeongshin/Team5_web/model.py
@@ -5,6 +5,15 @@ class Image(db.Model):
     image = db.Column(db.LargeBinary, nullable=False)
     caption = db.Column(db.Text, nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
-    #
-    # def __repr__(self):
-    #     return f'Image(id={self.id}, caption="{self.caption[:20]}")'
+
+    def __repr__(self):
+        return f'Image(image={self.image}, caption="{self.caption}")'
+
+class Braille(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    braille_image = db.Column(db.LargeBinary, nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
+    def __repr__(self):
+        return f'Braille(braille={self.braille_image}, text="{self.text}")'
--- Yeongshin/Team5_web/views/main.py
+++ Yeongshin/Team5_web/views/main.py
@@ -5,44 +5,53 @@ from PIL import Image
 bp = Blueprint('main', __name__, url_prefix='/')
 engine = pyttsx3.init()
 from Team5_web.utils import image_to_binary
-from Team5_web.model import Image
+from Team5_web.model import Image, Braille
+
 
 def generate_caption(image):
     # 모델 코드 구현
     caption = "예시 캡션: 이미지에 대한 설명"
     return caption
 
+def text_to_braille_image(text):
+    # 모델 코드 구현
+    braille_image = None
+    return braille_image
+
 @bp.route('/', methods=['GET', 'POST'])
 def index():
+    braille_image = None
     if request.method == 'POST':
-        # 요청에서 이미지 가져오기
-        image_file = request.files['image']
-        image = Image.open(image_file.stream)
+        if 'image' in request.files:
+            # 요청에서 이미지 가져오기
+            image_file = request.files['image']
+            image = Image.open(image_file.stream)
 
-        # 캡션 생성
-        caption = generate_caption(image)
+            # 캡션 생성
+            caption = generate_caption(image)
 
-        # 이미지를 바이너리 데이터로 변환
-        image_binary = image_to_binary(image)
+            # 이미지를 바이너리 데이터로 변환
+            image_binary = image_to_binary(image)
 
-        # 이미지와 캡션을 데이터베이스에 저장
-        new_image = Image(image=image_binary, caption=caption)
-        db.session.add(new_image)
-        db.session.commit()
+            # 이미지와 캡션을 데이터베이스에 저장
+            new_image = Image(image=image_binary, caption=caption)
+            db.session.add(new_image)
+            db.session.commit()
 
-        # 캡션을 음성으로 변환
-        engine.say(caption)
-        engine.runAndWait()
+            # 캡션을 음성으로 변환
+            engine.say(caption)
+            engine.runAndWait()
 
-    #     flash('Image uploaded and caption generated successfully.', 'success')
-    #     return redirect(url_for('main.index'))
-    #
-    # return render_template('index.html')
+            return render_template('index.html', caption=caption)
+        elif 'text' in request.form:
+            text = request.form['text']
+            braille_image = text_to_braille_image(text)
 
-        return render_template('index.html', caption=caption)
-    return render_template('index.html')
+    return render_template('index.html', braille_image=braille_image)
+
 
 @bp.route('/gallery')
 def gallery():
     images = db.session.query(Image).all()
-    return render_template('gallery.html', images=images)
+    brailles = db.session.query(Braille).all()
+    return render_template('gallery.html', images=images, brailles=brailles)
